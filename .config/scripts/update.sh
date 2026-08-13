e3e7f8b2617718a9b248ef2722bc75883e745c83
sklavier <emilio.programando@gmail.com>
2026-08-12 21:48:01 -0400

    better update script

--- .config/scripts/update.sh
+++ .config/scripts/update.sh
@@ -1,8 +1,45 @@
 #!/bin/bash
+
 clear
 echo "#################### Updating... #######################"
 
-sudo pacman -Syyu --noconfirm
-yay -Syyu --noconfirm
+yay -Syu --noconfirm
 flatpak update -y
 hyprpm update
+
+echo ""
+while true; do
+    read -p "Clean before update? [Y/n]" respuesta
+    
+    respuesta_LC="${respuesta,,}"
+
+    if [[ -z "$respuesta" || "$respuesta_LC" == "y" ]]; then
+        break
+    elif [[ "$respuesta_LC" == "n" ]]; then
+        echo "updated! not proceding with a clean"
+        exit 0
+    else
+        echo "idiot, press enter or 'y' to clean, 'for' done the program"
+    fi
+done
+
+clear
+
+echo "#################### Clearing... #######################"
+
+yay -Yc --noconfirm
+
+sudo pacman -Sc --noconfirm
+yay -Scc --noconfirm
+
+flatpak uninstall --unused -y
+
+sudo journalctl --vacuum-size=50M
+
+echo ""
+echo "############## (keeping wal folder)... #################"
+find "$HOME/.cache" -mindepth 1 -maxdepth 1 ! -name 'wal' -exec rm -rf {} +
+
+sudo paccache -rk1
+
+echo "Ok!"
